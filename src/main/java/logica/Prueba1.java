7830647b99c5982e0cf73a50bb348eb805582f86
rlovece <ruth.lovece@gmail.com>
2023-05-02 12:40:09 -0300

    first commit

---
+++ src/main/java/logica/Prueba1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package logica;
+import GUI.Pantalla;
+
+/**
+ *
+ * @author Ruth
+ */
+public class Prueba1 {
+
+    public static void main(String[] args) {
+        Pantalla pant1 = new Pantalla();
+        pant1.setVisible(true);
+        pant1.setLocationRelativeTo(null);
+        
+    }
+}
